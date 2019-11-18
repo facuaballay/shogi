2dee62d4762c614446fa301026e5098c23a8b235
unknown <flacomza09@gmail.com>
2019-11-18 11:02:01 -0300

    formulario html,funciones de mover,eliminar y buscar fichas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TableroService } from './service/tablero.service';
 
 @Component({
   selector: 'app-root',
@@ -7,176 +8,81 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'shogi';
+  tablero:Array<any>;
+  lugar: string;
+  ficha: string;
+  fila: string ='';
+  columna: string = '';
+  filaEliminar: number;
+  columnaEliminar: number;
+  jugador = "jugador1";
+  lista;
 
-  tablero = new Array(9);
-  constructor(){
+  constructor(public TableroService:TableroService){
 
-    this.CrearTablero();
+    this.tablero = this.TableroService.CrearTablero();
+    this.lista = TableroService.fichas;
+
   }
 
   ngOnInit() {
   }
-
-  fichas = [
-    {id:"ficha1",
-    nombre:'Lancero',
-    permiso:''
-    },
-    {id:"ficha2",
-    nombre:'Caballo',
-    permiso:''
-    },
-    {id:"ficha3",
-    nombre:'G.Plata',
-    permiso:''
-    },
-    {id:"ficha4",
-    nombre:'G.Oro',
-    permiso:''
-    },
-    {id:"ficha5",
-    nombre:'Rey',
-    permiso:''
-    },
-    {id:"ficha6",
-    nombre:'G.Oro',
-    permiso:''
-    },
-    {id:"ficha7",
-    nombre:'G.Plata',
-    permiso:''
-    },
-    {id:"ficha8",
-    nombre:'Caballo',
-    permiso:''
-    },
-    {id:"ficha9",
-    nombre:'Lancero',
-    permiso:''
-    },
-    {id:"ficha10",
-    nombre:'Torre',
-    permiso:''
-    },
-    {id:"ficha11",
-    nombre:'Alfil',
-    permiso:''
-    },
-    {id:"ficha12",
-    nombre:'Peon',
-    permiso:''
-    },
-    {id:"ficha13",
-    nombre:'Peon',
-    permiso:''
-    },
-    {id:"ficha14",
-    nombre:'Peon',
-    permiso:''
-    },
-    {id:"ficha15",
-    nombre:'Peon',
-    permiso:''
-    },
-    {id:"ficha16",
-    nombre:'Peon',
-    permiso:''
-    },
-    {id:"ficha17",
-    nombre:'Peon',
-    permiso:''
-    },
-    {id:"ficha18",
-    nombre:'Peon',
-    permiso:''
-    },
-    {id:"ficha19",
-    nombre:'Peon',
-    permiso:''
-    },
-    {id:"ficha20",
-    nombre:'Peon',
-    permiso:''
-    },
-
-
-  ]
-//tablero 9x9
-//creacion del tablero.
-CrearTablero(){
-  let contador = 0;
-  let fichasLugar = -1;
-
-  for (let index = 0; index < 9; index++) {
-    this.tablero[index] = new Array(9);
-  }  
-
+//buscar la ficha
+onFicha(ficha: string):boolean{
+   
   for(var f=0;f<9;f++){
     for(var c=0;c<9;c++){
-      if(contador < 9  ){
-        this.tablero[f][c]= {
-          id: this.fichas[++fichasLugar].id, 
-          nombre: this.fichas[fichasLugar].nombre,
-          permiso: this.fichas[fichasLugar].permiso = "jugador1"
-          };
-      //tablero del 0 al 9    
-      }
-      else if(contador === 10 || contador === 16){
-        this.tablero[f][c]= {
-          id: this.fichas[++fichasLugar].id, 
-          nombre: this.fichas[fichasLugar].nombre,
-          permiso: this.fichas[fichasLugar].permiso = "jugador1"
-          };
-      //tablero del 10 y 16    
-      }
-       else if(  contador >= 11 && contador <= 15 ){
-          this.tablero[f][c]= {
-            id:'', 
-            nombre: ''
-          };                 
-        //tablero del 11 y 15    
-        }
-       else if (contador > 17 && contador < 27){
-         this.tablero[f][c]= {
-           id: this.fichas[++fichasLugar].id, 
-           nombre: this.fichas[fichasLugar].nombre,
-           permiso: this.fichas[fichasLugar].permiso = "jugador1"
-          }; 
-       //tablero del 17 y 27      
-       }
-      else if(contador > 53 && contador < 63){
-        this.tablero[f][c]= {
-            id: this.fichas[fichasLugar].id, 
-            nombre: this.fichas[fichasLugar--].nombre,
-            permiso: this.fichas[fichasLugar].permiso = "jugador2"
-          };
-      //tablero del 53 y 63                        
-      }
-      else if(contador == 64 || contador === 70){
-        this.tablero[f][c]= {
-          id: this.fichas[fichasLugar].id, 
-          nombre: this.fichas[fichasLugar--].nombre,
-          permiso: this.fichas[fichasLugar].permiso = "jugador2"
-         };
-      //tablero del 64 y 70   
-      }
-      else if(contador > 71 && contador < 81){
-        this.tablero[f][c]= {
-          id: this.fichas[fichasLugar].id, 
-          permiso: this.fichas[fichasLugar].permiso = "jugador2",
-          nombre: this.fichas[fichasLugar--].nombre
-         };
-      //tablero del 71 y 81    
-      }
-      else{
-        this.tablero[f][c]={id:"", nombre: ""};
-        }
-      contador++
-      //total 81.
-      }
+      if(this.tablero[f][c].id === ficha && this.tablero[f][c].permiso === this.jugador ){
+        this.filaEliminar = f;
+        this.columnaEliminar = c;
+        this.ficha = this.tablero[f][c];
+        console.log(this.jugador)
+        return true;
+      } 
     }
-  console.log(this.tablero)
   }
+return false;
+}
+//elimina ficha
+onEliminar():void{
+  this.tablero[this.filaEliminar][this.columnaEliminar] = {id:"", nombre: ""};
+}
+//lugar ficha
+onLugar(lugar:string){
+  this.fila = lugar.substr(0,1);
+  this.columna = lugar.substr(1,1);
+  this.tablero[this.fila][this.columna] = this.ficha;
+
+  }
+//Mueve Ficha  
+onMover(ficha: string, lugar:string){
+  
+  let EstaFicha = this.onFicha(ficha);
+  
+  if(EstaFicha){
+   
+      this.onEliminar();
+      this.onLugar(lugar);
+      switch (this.jugador) {
+        case this.jugador = "jugador1":
+            this.jugador = "jugador2"
+            alert('turno del jugador 2')
+          break;
+          case this.jugador = "jugador2":
+            this.jugador = "jugador1"
+            alert('turno del jugador 1')
+          break;
+        default:
+          break;
+      }
+
+  }else{
+    console.log('te mandaste cualquiera')
+    
+  }
+  
+}
+
 }
 
 
---
+++ src/app/service/tablero.service.ts
@@ -0,0 +1,197 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TableroService {
+ 
+  tablero= new Array(9);
+  fichas = [
+    {id:"1",
+    nombre:'Lancero',
+    img:'assets/Fichas/Lancero.png',
+    permiso:''
+    },
+    {id:"2",
+    nombre:'Caballo',
+    img:'assets/Fichas/Lancero.png',
+    permiso:''
+    },
+    {id:"3",
+    nombre:'G.Plata',
+    img:'assets/Fichas/G.plata.png',
+    permiso:''
+    },
+    {id:"4",
+    nombre:'G.Oro',
+    img:'assets/Fichas/G.oro.png',
+    permiso:''
+    },
+    {id:"5",
+    nombre:'Rey',
+    img:'assets/Fichas/Rey.png',
+    permiso:''
+    },
+    {id:"6",
+    nombre:'G.Oro',
+    img:'assets/Fichas/G.oro.png',
+    permiso:''
+    },
+    {id:"7",
+    nombre:'G.Plata',
+    img:'assets/Fichas/G.plata.png',
+    permiso:''
+    },
+    {id:"8",
+    nombre:'Caballo',
+    img:'assets/Fichas/Caballo.png',
+    permiso:''
+    },
+    {id:"9",
+    nombre:'Lancero',
+    img:'assets/Fichas/Lancero.png',
+    permiso:''
+    },
+    {id:"10",
+    nombre:'Torre',
+    img:'assets/Fichas/Torre.png',
+    permiso:'',
+    },
+    {id:"11",
+    nombre:'Alfil',
+    img:'assets/Fichas/Alfil.png',
+    permiso:''
+    },
+    {id:"12",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    },
+    {id:"13",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    },
+    {id:"14",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    },
+    {id:"15",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    },
+    {id:"16",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    },
+    {id:"17",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    },
+    {id:"18",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    },
+    {id:"19",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    },
+    {id:"20",
+    nombre:'Peon',
+    img:'assets/Fichas/Peon.png',
+    permiso:''
+    }
+  ]
+
+  constructor() { }
+
+  CrearTablero(){
+//tablero 9x9
+//creacion del tablero.
+  let contador = 0;
+  let fichasLugar = -1;
+
+  for (let index = 0; index < 9; index++) {
+    this.tablero[index] = new Array(9);
+  }  
+
+  for(var f=0;f<9;f++){
+    for(var c=0;c<9;c++){
+      if(contador < 9  ){
+        this.tablero[f][c]= {
+          id: this.fichas[++fichasLugar].id, 
+          nombre: this.fichas[fichasLugar].nombre,
+          img:this.fichas[fichasLugar].img,
+          permiso: this.fichas[fichasLugar].permiso = "jugador1"
+          };
+      //tablero del 0 al 9    
+      }
+      else if(contador === 10 || contador === 16){
+        this.tablero[f][c]= {
+          id: this.fichas[++fichasLugar].id, 
+          nombre: this.fichas[fichasLugar].nombre,
+          img:this.fichas[fichasLugar].img,
+          permiso: this.fichas[fichasLugar].permiso = "jugador1"
+          };
+      //tablero del 10 y 16    
+      }
+       else if(  contador >= 11 && contador <= 15 ){
+          this.tablero[f][c]= {
+            id:'', 
+            nombre: ''
+          };                 
+        //tablero del 11 y 15    
+        }
+       else if (contador > 17 && contador < 27){
+         this.tablero[f][c]= {
+           id: this.fichas[++fichasLugar].id, 
+           nombre: this.fichas[fichasLugar].nombre,
+           img:this.fichas[fichasLugar].img,
+           permiso: this.fichas[fichasLugar].permiso = "jugador1"
+          }; 
+       //tablero del 17 y 27      
+       }
+      else if(contador > 53 && contador < 63){
+        this.tablero[f][c]= {
+            id: this.fichas[fichasLugar].id, 
+            permiso: this.fichas[fichasLugar].permiso = "jugador2",
+            img:this.fichas[fichasLugar].img,
+            nombre: this.fichas[fichasLugar--].nombre,
+          };
+      //tablero del 53 y 63                        
+      }
+      else if(contador == 64 || contador === 70){
+        this.tablero[f][c]= {
+          id: this.fichas[fichasLugar].id, 
+          permiso: this.fichas[fichasLugar].permiso = "jugador2",
+          img:this.fichas[fichasLugar].img,
+          nombre: this.fichas[fichasLugar--].nombre,
+         };
+      //tablero del 64 y 70   
+      }
+      else if(contador > 71 && contador < 81){
+        this.tablero[f][c]= {
+          id: this.fichas[fichasLugar].id, 
+          permiso: this.fichas[fichasLugar].permiso = "jugador2",
+          img:this.fichas[fichasLugar].img,
+          nombre: this.fichas[fichasLugar--].nombre
+         };
+      //tablero del 71 y 81    
+      }
+      else{
+        this.tablero[f][c]={id:"", nombre: ""};
+        }
+      contador++
+      //total 81.
+      }
+    }
+    return this.tablero;
+  }
+}
+
